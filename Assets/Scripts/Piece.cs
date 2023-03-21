533075f0ac41dd81d4cb17ad59e8a793eb3fd248
Jesus Noland <jesus.noland@litpen.com>
2023-03-20 20:57:55 -0700

    Added enum for piece type

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,6 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+public enum PieceTypes
+{
+    Deborah = 0,
+    Elisha = 1,
+    Lamb = 2,
+    Andrew = 3
+}
 
 public class Piece
 {
@@ -9,21 +15,36 @@ public class Piece
 
     private Vector3 position;
     private Vector2 gridPosition;
+    private PieceTypes pieceType;
 
     public Piece()
     {
         position = Vector3.zero;
         gridPosition = Vector2.zero;
+        pieceType = PieceTypes.Andrew;
     }
 
     public Piece(Vector3 position, Vector2 gridPosition)
     {
         this.position = position;
         this.gridPosition = gridPosition;
+        this.pieceType = PieceTypes.Andrew;
+    }
+
+    public Piece(Vector3 position, Vector2 gridPosition, PieceTypes pieceType)
+    {
+        this.position = position;
+        this.gridPosition = gridPosition;
+        this.pieceType = pieceType;
     }
 
     public Vector3 GetPosition()
     {
         return position;
+    }
+
+    public PieceTypes GetPieceType()
+    {
+        return pieceType;
     }
 }
